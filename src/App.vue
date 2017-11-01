85d8315f18f5dea04ec840db5a6404deb87b3d23
John <johnmcguin88@gmail.com>
2017-10-31 21:22:30 -0600

    began to componentize scss a little bit and added some base styles along with a small amount of started content for the about page

--- src/App.vue
+++ src/App.vue
@@ -23,90 +23,20 @@ export default {
 </script>
 
 <style lang="scss">
+
+@import "./assets/styles/colors.scss";
+@import "./assets/styles/base.scss";
+@import "./assets/styles/layout.scss";
+@import "./assets/styles/animations.scss";
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  font-size: 1.6rem;
-  margin: 60px auto 0 auto;
-  max-width: 960px;
-}
-
-.page-turn-enter, .page-turn-enter-to {
-  opacity: 0;
-}
-
-.page-turn-enter-active {
-  animation: fade-in .5s;
-  animation-delay: .5s;
-}
-
-.page-turn-leave-active {
-  animation: slide-out .5s;
-  animation-timing-function: ease-out;
-}
-
-html {
-  font-size: 10px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-
-@keyframes fade-in {
-  0% {
-    opacity: 0;
-  }
-  25% {
-    opacity: .25;
-  }
-  50% {
-    opacity: .5;
-  }
-  75% {
-    opacity: .75;
-  }
-  100% {
-    opacity: 1;
-  }
-}
-
-@keyframes slide-out {
-  0% {
-    transform: translateX(0);
-  }
-  25% {
-    transform: translateX(-25%);
-    opacity: .25;
-  }
-  50% {
-    transform: translateX(-50%);
-    opacity: .5
-  }
-  75% {
-    transform: translateX(-75%);
-    opacity: .75;
-  }
-  100% {
-    transform: translateX(-100%);
-    opacity: 0;
-  }
+  color: $font-gray;
+  font-size: 1.8rem;
+  margin: auto;
+  line-height: 1.5;
 }
 </style>
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,9 +1,29 @@
 <template>
-    <h1>Hello About Page</h1>
+  <div>
+    <h1>About The Way</h1>
+    <p>
+      Thank you for visiting! The Way is a one person development shop that delivers software solutions
+      for you business needs. I specialize in web development but appreciate the opportunity to learn new
+      technology. After graduating from a bootcamp in 2013, I spent a year churning out websites for an
+      agency based in Boulder, CO. It was here that I started to appreciate UI/UX design and development 
+      and began to hone skills in building interactive and beatiful User Interfaces.
+    </p>
+    <p>
+      I learned a lot at that job and knew that I liked building beautiful sites, but also knew I wanted more. 
+      After that year, I started contracting at a small company based in Denver, CO which provided me to 
+      work as a full stack developer and grow into a more well-rounded developer. I greatly enjoy working on 
+      this small team and learn a lot every day, but that same curiosity and desire for growth has led me here, 
+      to seek new client work.
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'About'
-}
+  name: "About"
+};
 </script>
+
+<style lang="scss">
+@import "../assets/styles/layout.scss";
+</style>
