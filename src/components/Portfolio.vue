9b81e23bc34ada742559a5c09a271a1c38123b0f
John <johnmcguin88@gmail.com>
2017-11-05 21:25:28 -0700

    simple list view of projects, unstyled, just learning vue importing a js file and iterating over the data

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,9 +1,20 @@
 <template>
+  <div>
     <h1>Hello Portfolio Page</h1>
+    <ul>
+      <li v-for="project in projects" v-bind:key="project.id">{{project.name}}</li>
+    </ul>
+    <p>**definitely needs styling, just getting a simple list view rendered from a projects.js file</p>
+  </div>
 </template>
 
 <script>
+import projects from "../model/projects";
+
 export default {
-  name: 'Portfolio'
-}
+  name: "Portfolio",
+  data() {
+    return { projects };
+  }
+};
 </script>
---
+++ src/model/projects.js
@@ -0,0 +1,28 @@
+const projects = [
+    {
+        name: "Enli Health Intelligence",
+        id: 1,
+    },
+    {
+        name: "Common",
+        id: 2,
+    },
+    {
+        name: "Oceanic Preservation Society",
+        id: 3,
+    },
+    {
+        name: "HDS Doorways",
+        id: 4,
+    },
+    {
+        name: "HDS Inspections iOS",
+        id: 5,
+    },
+    {
+        name: "HDS Work Orders iOS",
+        id: 6
+    }
+];
+
+export default projects;
