ba05daba08a4e4d1b113f26ffe00359d80bbfd4c
John <johnmcguin88@gmail.com>
2017-11-06 18:41:46 -0700

    basic list of services

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,5 +1,13 @@
 <template>
+  <div>
     <h1>Hello Services Page</h1>
+    <ul>
+      <li>web development (blogs, sites, shops, small apps) *specializing in front-end development but can implement full-stack solutions</li>
+      <li>mobile development</li>
+      <li>site maintenance</li>
+      <li>design</li>
+    </ul>
+  </div>
 </template>
 
 <script>
