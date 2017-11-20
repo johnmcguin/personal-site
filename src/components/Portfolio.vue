032ef5d3634ddd0f30143e6e4f8531e0b6fca53a
John <johnmcguin88@gmail.com>
2017-11-19 19:47:15 -0700

    projects related stuff

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -6,9 +6,13 @@
         <div class="img" v-bind:style="{ backgroundImage: 'url(' + project.img + ')'}"></div>
 
         <div class="description">
-          <h3>{{project.title}}</h3>
-          <h6>{{project.subTitle}}</h6>
+          <div>
+            <h3>{{project.title}}</h3>
+            <h6>{{project.subTitle}}</h6>
+            <p>{{project.description}}</p>
+          </div>
           <ul>
+            <li>Tech Stack:</li>
             <li v-for="(tech, idx) in project.technologies" v-bind:key="idx">
               {{tech}}<span v-if="idx + 1 < project.technologies.length">, </span>
             </li>
@@ -33,12 +37,15 @@ export default {
 <style lang="scss">
 .grid {
   display: grid;
-  grid-template-columns: repeat(3, 1fr); // fr is a fraction unit, so this just says "make 3 columns that are of equal width"
-  grid-gap: 1rem;
+  grid-template-columns: repeat(3, 1fr); 
+  grid-gap: 2rem;
 }
 
 .project {
   box-shadow: 0px 0px 2px 0px rgba(150, 150, 150, 0.9);
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 
   .img {
     width: 100%;
@@ -49,15 +56,20 @@ export default {
 
   .description {
     padding: 1rem;
+    flex: 1 0 auto;
     display: flex;
     flex-direction: column;
+    justify-content: space-between;
     align-items: flex-start;
-    justify-content: flex-start;
   }
 
   h3, h6 {
     text-align: left;
     margin: 0px;
+  }
+
+  p {
+    text-align: left;
   }
 
   ul {
