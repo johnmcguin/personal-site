7cce612cacc57770af4586ad8e29ba72f39f110f
John <johnmcguin88@gmail.com>
2017-12-07 16:38:23 -0700

    modify about description

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,9 +2,11 @@
   <div class="about">
     <p>
       The Way Software Development specializes in web and mobile development, 
-      valuing simplicity, elegance, functionality, and beautiful UX / UI above all. 
+      valuing simplicity, elegance, functionality, intuitive UX, and beatiful UI. 
       I can help provide solutions for web sites, web applications, 
-      mobile applications, online stores, CMS's, and more.
+      mobile applications, online stores, CMS's, and more. I consider myself a 
+      Jack of all trades, and pursue mastery in modern front-end web development. 
+      Let's work together to solve your problems and create something great!
     </p>
   </div>
 </template>
