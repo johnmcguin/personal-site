06bcf38a5ed9689943308b81262f67f1da3e8881
John <johnmcguin88@gmail.com>
2017-12-07 16:38:54 -0700

    small portfolio and service modifications

--- src/model/services.js
+++ src/model/services.js
@@ -12,16 +12,10 @@ const services = [
         id: 2
     },
     {
-        name: 'Web Design',
-        description: 'My primary focus is in custom web development and I would be glad to implement a pre-existent design, but maybe you would like to save some money by allowing me to do both design and development for your project.',
-        icon: require('../assets/icons/svg/interface.svg'),
-        id: 3
-    },
-    {
         name: 'Desktop Development',
         description: 'Modern web technologies allow us to write the same code that we write for the web to create desktop and native mobile OS applications.',
         icon: require('../assets/icons/svg/technology-1.svg'),
-        id: 4
+        id: 3
     }
 ];
 
